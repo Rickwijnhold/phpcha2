242c9ba9eab2a49da282e227e89fa6043b4f90af
RIckwijnhold <rick.wijnhold@student.rocva.nl>
2019-10-14 11:06:38 +0200

    opdracht 2

---
+++ opdracht2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Opdracht 2</title>
+</head>
+<style>
+    body{
+    text-align: center;
+        font-family: Arial;
+        font-weight: bold;
+        font-size: 30px;
+    }
+    li {
+        color: #ff8400;
+        font-family: "Arial Rounded MT Bold";
+        list-style-type: none;
+    }
+</style>
+<body>
+<img src="img/monkey-business.jpg">
+<p>Select your monkey!</p>
+<img src="img/monkey_swings.png">
+<?php
+$aap1 = "BAVIAAN";?>
+<a href="https://www.google.nl/search?q=Baviaan&tbm=isch"><?php echo "<br>". $aap1; ?></a>
+<?php
+$aap2 = "GUEREZA";?>
+<a href="https://www.google.nl/search?q=Guereza&tbm=isch"><?php echo "<br>".$aap2; ?></a>
+<?php
+$aap3 = "LANGOER";?>
+<a href="https://www.google.nl/search?q=Langoer&tbm=isch"><?php echo "<br>".$aap3; ?></a>
+<?php
+$aap4 = "NEUSAAP";?>
+<a href="https://www.google.nl/search?q=Neusaap&tbm=isch"><?php echo "<br>".$aap4; ?></a>
+<?php
+$aap5 = "TAMARIN";?>
+<a href="https://www.google.nl/search?q=Tamarin&tbm=isch"><?php echo "<br>".$aap5; ?></a>
+<?php
+$aap6 = "BRULAAP";?>
+<a href="https://www.google.nl/search?q=Brulaap&tbm=isch"><?php echo "<br>".$aap6; ?></a>
+<?php
+$aap7 = "HALFAAP";?>
+<a href="https://www.google.nl/search?q=Halfaap&tbm=isch"><?php echo "<br>".$aap7; ?></a>
+<?php
+$aap8 = "MANDRIL";?>
+<a href="https://www.google.nl/search?q=Mandril&tbm=isch"><?php echo "<br>".$aap8; ?></a>
+</body>
+</html>
+
